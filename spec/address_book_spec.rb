1460332fba794d08ca6a6315d0b0945d765b609e
Your Name <joe.scalera@gmail.com>
2015-08-13 09:33:52 -0700

    csv homework

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -83,15 +83,38 @@ require_relative '../models/address_book.rb'
        check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
 
      end
+     def check_entry(entry, expected_name, expected_number, expected_email)
+         expect(entry.name).to eql expected_name
+         expect(entry.phone_number).to eql expected_number
+         expect(entry.email).to eql expected_email
+       end
 
-     it "imports the 5th entry" do
-       book.import_from_csv("entries.csv")
-       # Check the fifth entry
-       entry_five = book.entries[4]
-       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+        context ".import_from_csv" do
+         it "imports the correct number of entries" do
+       # #3
+           book.import_from_csv("entries_2.csv")
+           book_size = book.entries.size
+
+           # Check the size of the entries in AddressBook
+           expect(book_size).to eql 3
+     end
+     it "imports the 6th entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_six = book.entries[0]
+       check_entry(entry_six, "Arnold","777-421-5079", "heyarnold@bloc.com")
 
      end
-
+    it "imports the 7th entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_seven = book.entries[1]
+      check_entry(entry_seven, "Gerald","457-899-4800","ballislife@yahoo.com")
+    end
+    it "imports the 8th entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_eight = book.entries[2]
+      check_entry(entry_eight, "Kobe", "356-990-3467","coffedrank@uplate.com")
+    end
    end
+end
 end
 end
